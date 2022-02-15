b97d63526bfc2b7ca427110b8fe643efc6c713e2
gibrankasif <gibran@vetstoria.com>
2022-02-15 12:11:05 +0530

    removed bower dependencies.

--- scripts/karma.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-./node_modules/.bin/karma start karma/karma.conf-$1.js --single-run
--- scripts/protractor.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-node server.js&>application.log
-./node_modules/protractor/bin/protractor karma/protractor.conf.js
